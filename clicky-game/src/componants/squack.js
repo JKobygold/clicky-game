434e6945c4cc3f2764a8b4918b0444e39e0d0486
Jacob Gold <jkobygold@gmail.com>
2020-05-05 19:48:56 -0400

    Updates

--- clicky-game/src/componants/squack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function squack() {
-    return (
-        <div>
-            Who are you to think so highy
-        </div>
-    )
-}
---
+++ precache-manifest.df73228b8ef2c4dca045246aa3839f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0bbf580a448370d9127687bf5410a5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "38f0f2b173dbccc138ac",
+    "url": "/clicky-game/static/css/2.16c59a73.chunk.css"
+  },
+  {
+    "revision": "3e019333077bc16add20",
+    "url": "/clicky-game/static/css/main.f66ee3c6.chunk.css"
+  },
+  {
+    "revision": "38f0f2b173dbccc138ac",
+    "url": "/clicky-game/static/js/2.568784bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.568784bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e019333077bc16add20",
+    "url": "/clicky-game/static/js/main.74f28bd1.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
